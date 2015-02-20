ca680c644d462ee7d46f4e2d71e30ad27151fe09
joshualehr <joshuadlehr@hotmail.com>
2015-02-19 22:36:47 -0500

    Added and configured nHibernate

---
+++ SimpleBlog/Areas/Admin/ViewModels/Users.cs
@@ -0,0 +1,11 @@
+﻿using SimpleBlog.Models;
+using System.Collections.Generic;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class UsersIndex
+    {
+        public IEnumerable<User> Users { get; set; }
+
+    }
+}
