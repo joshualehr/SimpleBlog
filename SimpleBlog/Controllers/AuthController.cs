700e46998e4dd80f95e199af8de9b3681b9983e1
joshualehr <joshuadlehr@hotmail.com>
2015-02-24 21:28:09 -0500

    Added database driving authentication

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,10 +1,12 @@
-﻿using SimpleBlog.ViewModels;
+﻿using SimpleBlog.Models;
+using SimpleBlog.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using NHibernate.Linq;
 
 namespace SimpleBlog.Controllers
 {
@@ -26,12 +28,20 @@ namespace SimpleBlog.Controllers
         [AllowAnonymous]
         public ActionResult Login(AuthLogin form, string returnUrl)
         {
+            var user = Database.Session.Query<User>().FirstOrDefault(u => u.Username == form.Username);
+
+            if (user == null)
+                SimpleBlog.Models.User.FakeHash();
+
+            if (user == null || !user.CheckPassword(form.Password))
+                ModelState.AddModelError("Username", "Username or password is not correct.");
+
             if (!ModelState.IsValid)
             {
                 return View(form);
             }
 
-            FormsAuthentication.SetAuthCookie(form.Username, true);
+            FormsAuthentication.SetAuthCookie(user.Username, true);
 
             if (!string.IsNullOrWhiteSpace(returnUrl))
             {
--- SimpleBlog/Infrastructure/RoleProvider.cs
+++ SimpleBlog/Infrastructure/RoleProvider.cs
@@ -9,7 +9,7 @@ namespace SimpleBlog.Infrastructure
     {
         public override string[] GetRolesForUser(string username)
         {
-            if (username == "nelson")
+            if (username == "Joshua")
             {
                 return new[] { "admin" };
             }
